05874550eb5687b935c3d22279cd19c909feb926
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-02-08 19:51:51 +0000

    Update: '0-uniq_users.sql' modified

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,2 +1,6 @@
 -- Creates a new table called users
-CREATE TABLE IF NOT EXISTS `users` (id INT PRIMARY KEY, email CHAR(255) UNIQUE, name CHAR(255));
+CREATE TABLE IF NOT EXISTS `users` (
+id INT PRIMARY KEY,
+email CHAR(255) UNIQUE NOT NULL,
+name CHAR(255)
+);

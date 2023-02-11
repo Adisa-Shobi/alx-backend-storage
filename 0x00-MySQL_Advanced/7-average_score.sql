359c474ec89a26e2b07940fe10d63c0692b5fbe6
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-02-11 10:56:15 +0000

    Update: '7-average_score.sql' bug fix

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -9,7 +9,7 @@ BEGIN
 
     SELECT AVG(score) INTO average_score
     FROM corrections
-    WHERE `user_id` = user_id;
+    WHERE corrections.user_id = user_id;
 
     UPDATE users
     SET average_score = average_score

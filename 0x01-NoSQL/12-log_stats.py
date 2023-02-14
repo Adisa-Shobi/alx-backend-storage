af4dc31870bfa8216a59f194ca0487e703cea931
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-02-14 14:11:15 +0000

    Update: '12-log_stats.py' modified

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,16 +5,24 @@
 from pymongo import MongoClient
 
 
-client = MongoClient()
-nginx_collection = client.logs.nginx
-no_logs = nginx_collection.count_documents({})
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-print("{} logs".format(no_logs))
-print("Methods:")
-for mthd in methods:
-    print("\tmethod {}: {}".format(mthd, nginx_collection.count_documents(
-        {"method": mthd}
+def log_summary():
+    '''
+    prints a summary of nginx log
+    '''
+    client = MongoClient()
+    nginx_collection = client.logs.nginx
+    no_logs = nginx_collection.count_documents({})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("{} logs".format(no_logs))
+    print("Methods:")
+    for mthd in methods:
+        print("\tmethod {}: {}".format(mthd, nginx_collection.count_documents(
+            {"method": mthd}
+        )))
+    print("{} status check".format(nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
     )))
-print("{} status check".format(nginx_collection.count_documents(
-    {"method": "GET", "path": "/status"}
-)))
+
+
+if __name__ == "__main__":
+    log_summary()
